aca417d353d1467ddf0f9e02400fa31ec89b7e63
Vikash Sharma <vs8824394430@gmail.com>
2024-02-06 23:35:51 +0530

    update port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ connectToMongo();
 const express = require("express");
 const app = express();
 var cors = require("cors");
-const port = process.env.REACT_APP_PORT || 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));

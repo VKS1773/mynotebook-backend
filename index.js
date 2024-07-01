2904971031ed34ce41e1dec0bc84436c6aff8070
Vikash Sharma <vs8824394430@gmail.com>
2024-07-02 00:01:38 +0530

    added changes

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ connectToMongo();
 const express = require("express");
 const app = express();
 var cors = require("cors");
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
-  console.log(`Mynotebook backend app listening on port ${process.env.URL}`);
+// app.listen(port, () => {
+//   console.log(`Mynotebook backend app listening on port ${process.env.URL}`);
+// });
+const hostname = "0.0.0.0";
+const port = 5000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
